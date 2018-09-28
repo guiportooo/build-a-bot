b8e4af24234a5174983f600a7e8a391d2dbdd836
Guilherme Porto <guiportooo@gmail.com>
2018-09-28 14:54:32 -0300

    Add routing support

--- src/App.vue
+++ src/App.vue
@@ -4,27 +4,28 @@
       <nav>
         <ul>
           <li class="nav-item">
+            <router-link class="nav-link" :to="{name: 'Home'}" exact>
             <img class="logo" src="./assets/build-a-bot-logo.png" />
             Build-a-Bot
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{name: 'Build'}">
+            Build
+            </router-link>
           </li>
         </ul>
       </nav>
     </header>
     <main>
-      <RobotBuilder />
+      <router-view />
     </main>
   </div>
 </template>
 
 <script>
-// import HomePage from './home/HomePage.vue';
-import RobotBuilder from './build/RobotBuilder.vue';
-
 export default {
   name: 'app',
-  components: {
-    RobotBuilder,
-  },
 };
 </script>
 
@@ -65,5 +66,12 @@ ul {
 .logo {
   vertical-align: middle;
   height: 30px;
+}
+.nav-link {
+  text-decoration: none;
+  color: inherit;
+}
+.router-link-active {
+  color: white;
 }
 </style>
