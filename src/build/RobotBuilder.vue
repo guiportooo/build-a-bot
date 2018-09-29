39bca144655b7e946ac183f3152b5f53dfa0c0b5
Guilherme Porto <guiportooo@gmail.com>
2018-09-29 18:27:24 -0300

    Prevent leaving from RobotBuilder if no robot has been added to cart

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -75,11 +75,22 @@ import CollapsibleSection from '../shared/CollapsibleSection.vue';
 
 export default {
   name: 'RobotBuilder',
+  beforeRouteLeave(to, from, next) {
+    if (this.addedToCart) {
+      next(true);
+    } else {
+      /* eslint no-alert: 0 */
+      /* eslint no-restricted-globals: 0 */
+      const response = confirm('You have not added your robot to your cart. Are you sure you want to leave?',);
+      next(response);
+    }
+  },
   components: { PartSelector, CollapsibleSection },
   data() {
     return {
       availableParts,
       cart: [],
+      addedToCart: false,
       selectedRobot: {
         head: {},
         leftArm: {},
@@ -104,6 +115,7 @@ export default {
         robot.rightArm.cost +
         robot.base.cost;
       this.cart.push(Object.assign({}, robot, { cost }));
+      this.addedToCart = true;
     },
   },
 };
