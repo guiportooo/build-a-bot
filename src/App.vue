d6b5fa5da61bd64080c098b4bfe6d7840fa62409
Guilherme Porto <guiportooo@gmail.com>
2018-09-29 18:09:49 -0300

    Add Sidebar components

--- src/App.vue
+++ src/App.vue
@@ -17,9 +17,14 @@
         </ul>
       </nav>
     </header>
-    <main>
-      <router-view />
-    </main>
+    <div class="container">
+      <aside>
+        <router-view name="sidebar" />
+      </aside>
+      <main>
+        <router-view />
+      </main>
+    </div>
   </div>
 </template>
 
@@ -37,20 +42,19 @@ body {
 </style>
 
 
-<style scoped>
+<style lang="scss" scoped>
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
 }
 main {
-  margin: 0 auto;
   padding: 30px;
   background-color: white;
-  width: 1024px;
+  width: 964px;
   min-height: 300px;
 }
 header {
   background-color: #999;
-  width: 1084px;
+  width: 1184px;
   margin: 0 auto;
 }
 ul {
@@ -73,5 +77,16 @@ ul {
 }
 .router-link-active {
   color: white;
+}
+.container {
+  display: flex;
+  margin: 10px auto 0 auto;
+  justify-content: center;
+  aside {
+    padding: 30px;
+    background-color: #aaa;
+    width: 100px;
+    min-height: 300px;
+  }
 }
 </style>
--- src/parts/BrowseParts.vue
+++ src/parts/BrowseParts.vue
@@ -7,7 +7,7 @@
       <li><router-link :to="{name: 'BrowseTorsos'}">Torsos</router-link></li>
       <li><router-link :to="{name: 'BrowseBases'}">Bases</router-link></li>
     </ul>
-    <router-view></router-view>
+    <router-view />
   </div>
 </template>
 
