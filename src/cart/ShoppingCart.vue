9047f76bb448fd1865c70a338ef71faf60a40914
Guilherme Porto <guiportooo@gmail.com>
2018-09-30 20:41:49 -0300

    Display cart items on sale on ShoppingCart component

--- src/cart/ShoppingCart.vue
+++ src/cart/ShoppingCart.vue
@@ -23,6 +23,30 @@
         </tr>
       </tbody>
     </table>
+
+    <h2 class="sale-items">You save money on these robots:</h2>
+    <table>
+      <thead>
+        <tr>
+          <th class="robot-title">
+            Robot
+          </th>
+          <th class="cost">
+            Cost
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(robot, index) in cartSaleItems" :key="index">
+          <td class="robot-title">
+            {{robot.head.title}}
+          </td>
+          <td class="cost">
+            {{robot.cost}}
+          </td>
+        </tr>
+      </tbody>
+    </table>
  </div>
 </template>
 
@@ -32,6 +56,9 @@ export default {
   computed: {
     cart() {
       return this.$store.state.cart;
+    },
+    cartSaleItems() {
+      return this.$store.getters.cartSaleItems;
     },
   },
 };
@@ -48,5 +75,10 @@ th {
 }
 .cost {
   text-align: right;
+}
+.sale-items {
+  margin-top: 50px;
+  font-size: 18px;
+  color: red;
 }
 </style>
